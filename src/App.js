b7acd5dbcda5e41898ac79455d9e8b85da831a95
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-04-04 21:39:30 +0200

    Fetch Weather Data From The Api - Modify getWeather Function That In [App.js] To Fetch The Data Through Api Key

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Form from './components/Form'
 import Weather from './components/Weather'
-function App() {
-  return (
-    <div className="App">
-      <Form/>
-      <Weather/>
-    </div>
-  );
+
+
+class App extends Component{
+    state={  
+      temp: '',
+      city:'',
+      country:'',
+      hum:'',
+      desc:''
+    }
+
+    getWeather = async e =>{
+      e.preventDefault()
+      let apiKey = 'ff40f98501e3093450157e4e24eba141',
+          city = e.target.elements.city.value, // Get City From The Form
+          country = e.target.elements.country.value; // Get Country From The Form
+
+      // If City And Country Not Empty It Will Fetch The Data
+      if(city !== '' && country !== ''){
+        const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`),
+            data = await api.json();
+        
+        this.setState({
+          temp: data.main.temp,
+          city:data.name,
+          country:data.sys.country,
+          hum:data.main.humidity,
+          desc:data.weather[0].description
+        })
+      
+      }else{
+        alert('Please Enter City And Country')
+      }
+    }
+
+    render(){
+      return (
+        <div className="App">
+          <Form getWeather={this.getWeather}/>
+          <Weather/>
+        </div>
+      );
+    }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Form extends Component {
-  render() {
-    return (
-      <div>
-        Form
-      </div>
-    );
-  }
+function Form(props){
+  return (
+    <div>
+      <form onSubmit={props.getWeather}>
+        <input type='text' name='city' placeholder='Enter City..'/>
+        <input type='text' name='country' placeholder='Enter Country...'/>
+        <button type='submit'>Get Weather</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
